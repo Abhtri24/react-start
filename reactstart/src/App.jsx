22b65c177e3cde9fd76e43ded4dd78ffaae55252
Abhinav <abhinavt244@gmail.com>
2025-01-17 00:34:18 +0530

    implementing some tailwind

---
+++ reactstart/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"; 
+import './index.css';
+import Card from "./components/card";
+
+
+  
+let myOb = {
+  
+}
+
+
+    function App() {
+      return(
+<>
+<Card />
+<Card/>
+       <h1 className="bg-green-800 text-black p-4 rounded-xl inline m-auto ">TEST</h1>
+        </>
+      )
+    }
+    
+
+export default App
